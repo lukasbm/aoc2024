07aab66749028b53f4aa679c916db82fca72ed30
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-02 13:50:05 +0100

    done day2

---
+++ 02/part2.hs
@@ -0,0 +1,27 @@
+parseReport :: [String] -> [Int]
+parseReport = map read
+
+main = do
+  raw_txt <- readFile "input.txt"
+  let reports = map (parseReport . words) $ lines raw_txt
+  let ratings = map evaluateReportTestAll reports
+  print $ foldl (\acc p -> (if p then acc + 1 else acc)) 0 ratings
+
+removeLevel :: [Int] -> Int -> [Int]
+removeLevel ls i = take i ls ++ drop (i + 1) ls
+
+evaluateReportTestAll :: [Int] -> Bool
+evaluateReportTestAll ls =
+  let perms = map (removeLevel ls) [0 .. (length ls - 1)]
+   in foldl (\acc l -> acc || evaluateReport l) False perms
+
+-- The levels are either all increasing or all decreasing.
+-- Any two adjacent levels differ by at least one and at most three.
+evaluateReport :: [Int] -> Bool
+evaluateReport [] = False
+evaluateReport ls =
+  let staggered = zip ls (tail ls)
+   in foldl (\acc (a, b) -> acc && abs (a - b) <= 3 && abs (a - b) >= 1) True staggered
+        && ( foldl (\acc (a, b) -> acc && (a > b)) True staggered
+               || foldl (\acc (a, b) -> acc && (a < b)) True staggered
+           )
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # aoc2024
+
 Advent of Code 2024 in Haskell
+
+## Tips
+
+```haskell
+main = do
+  line <- getLine
+  putStrLn line
+  main 
+```
+is the same as:
+```haskell
+main = getLine >>= putStrLn >> main
+```
