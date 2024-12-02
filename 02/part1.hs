6831d803647995bda83e914bbe4575b767a8f4d7
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-02 13:16:13 +0100

    part1 done

--- 02/part1.hs
+++ 02/part1.hs
@@ -2,9 +2,9 @@ parseReport :: [String] -> [Int]
 parseReport = map read
 
 main = do
-  raw_txt <- readFile "test_part1.txt"
+  raw_txt <- readFile "input.txt"
   let reports = map (parseReport . words) $ lines raw_txt
-  print $ map evaluateReport reports
+  print $ foldl (\acc p -> (if p then acc + 1 else acc)) 0 $ map evaluateReport reports
 
 -- The levels are either all increasing or all decreasing.
 -- Any two adjacent levels differ by at least one and at most three.
@@ -12,6 +12,7 @@ evaluateReport :: [Int] -> Bool
 evaluateReport [] = False
 evaluateReport ls =
   let staggered = zip ls (tail ls)
-   in foldl (\acc (a, b) -> acc && (abs (a - b) <= 3)) True staggered
-        || foldl (\acc (a, b) -> acc && (a > b)) True staggered
-        || foldl (\acc (a, b) -> acc && (a < b)) True staggered
+   in foldl (\acc (a, b) -> acc && abs (a - b) <= 3 && abs (a - b) >= 1) True staggered
+        && ( foldl (\acc (a, b) -> acc && (a > b)) True staggered
+               || foldl (\acc (a, b) -> acc && (a < b)) True staggered
+           )
