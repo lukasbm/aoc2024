8c4c396a400be88f19adb96df27e1d5005e342e3
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-03 14:52:34 +0100

    day 3 done

--- 03/part2.hs
+++ 03/part2.hs
@@ -9,22 +9,18 @@ findAllMatches pattern text = case matchRegexAll pattern text of
   Just (before, match, after, subs) -> filter (/= "") subs ++ findAllMatches pattern after
   Nothing -> []
 
-parseMatches :: [String] -> [(Int, Int)]
-parseMatches [] = []
-parseMatches ("do" : x : y : rest) = (read x, read y) : parseMatches rest
-parseMatches ("don't" : x : y : rest) = parseMatches rest
-parseMatches (x : y : rest) = parseMatches rest
-parseMatches _ = error "list not fully specified"
+parseMatches :: [String] -> Bool -> [(Int, Int)]
+parseMatches [] a = []
+parseMatches ("do" : x : y : rest) a = (read x, read y) : parseMatches rest True
+parseMatches ("don't" : x : y : rest) a = parseMatches rest False
+parseMatches (x : y : rest) True = (read x, read y) : parseMatches rest True
+parseMatches (x : y : rest) False = parseMatches rest False
+parseMatches _ a = error "list not fully specified"
 
 patternEq = mkRegex "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|(do)\\(\\)|(don't)\\(\\)"
 
 main :: IO ()
 main = readFile "input.txt" >>= print . sum . solve . intercalate "" . lines
 
--- main = do
---   raw_txt <- readFile "test_part2.txt"
---   let a = findAllMatches patternEq $ intercalate "" $ lines raw_txt
---   print $ parseMatches ("do" : a)
-
 solve :: String -> [Int]
-solve text = map (\(x, y) -> x * y) $ parseMatches ("do" : findAllMatches patternEq text)
+solve text = map (\(x, y) -> x * y) $ parseMatches (findAllMatches patternEq text) True
