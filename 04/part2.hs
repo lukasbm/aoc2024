b7927005d7ea61ebbc48dc48fced2532412b816d
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-05 00:51:17 +0100

    keep working on day 4 p2

--- 04/part2.hs
+++ 04/part2.hs
@@ -2,30 +2,27 @@ import Data.List (find, nub, transpose)
 import Data.Maybe (fromMaybe)
 import Prelude hiding (Left, Right)
 
-type Grid = [[Char]]
-
 cols :: [[a]] -> Int
 cols xss = length (head xss)
 
 rows :: [[a]] -> Int
 rows = length
 
-slidingWindow :: [[a]] -> (Int, Int) -> ([[a]] -> b) -> [b]
-slidingWindow xss (nrow, ncol) func
-  | rows xss < nrow = 0
-  | cols xss < ncol = 0
-slidingWindow xss size@(nrow, ncol) func =
-  [func (take nrow $ map (take ncol) xss)] ++ slidingWindow (drop 1 xss) size func ++ slidingWindow (map (drop 1) xss) size func
+slidingWindow :: (Int, Int) -> ([[a]] -> b) -> [[a]] -> [b]
+slidingWindow (nrow, ncol) func xss
+  | rows xss < nrow = []
+  | cols xss < ncol = []
+slidingWindow size@(nrow, ncol) func xss =
+  let window = take nrow $ map (take ncol) xss
+   in [func window] ++ slidingWindow size func (drop 1 xss) ++ slidingWindow size func (map (drop 1) xss)
 
 main :: IO ()
-main = readFile "input.txt" >>= print . sum . map count . nub . allText . lines
+main = readFile "input.txt" >>= print . slidingWindow (3, 3) xmas . lines
 
-matches2d_2 :: (Eq a) => [[a]] -> [[a]] -> Int
-matches2d_2 pattern xss | shorter xss pattern = 0 -- not enough rows
-matches2d_2 pattern (xs : _) | shorter xs (head pattern) = 0 -- not enough columns
-matches2d_2 pattern xss =
-  sum [1 | all and (zipWith (zipWith (==)) pattern xss)] -- this match
-    + matches2d_2 pattern (drop 1 xss) -- next row
-    + matches2d_2 pattern (map (drop 1) xss) -- next col
-
-shorter a b = length a < length b
+xmas :: [[Char]] -> Bool
+-- xmas [[tl, t, tr], [ml, m, mr], [bl, b, br]]
+xmas [['M', t, 'M'], [ml, 'A', mr], ['S', b, 'S']] = True -- Ms on top
+xmas [['M', t, 'S'], [ml, 'A', mr], ['M', b, 'S']] = True -- Ms on left
+xmas [['S', t, 'M'], [ml, 'A', mr], ['S', b, 'M']] = True -- Ms on right
+xmas [['S', t, 'S'], [ml, 'A', mr], ['M', b, 'M']] = True -- Ms on bot
+xmas _ = False
