c75c7a61a5b353b3632f9df7c38318f760b56884
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-07 03:54:02 +0100

    parsing and printing

--- 06/part1.hs
+++ 06/part1.hs
@@ -1,6 +1,23 @@
-data Object = GuardLeft | GuardRight | GuardUp | GuardDown | Free | Obstacle | Patrolled deriving (Show, Eq)
+{-# LANGUAGE FlexibleInstances #-}
 
-type Grid = [[Object]]
+import Data.List (intercalate)
+
+data Object = GuardLeft | GuardRight | GuardUp | GuardDown | Free | Obstacle | Patrolled deriving (Eq)
+
+instance Show Object where
+  show GuardLeft = "<"
+  show GuardRight = ">"
+  show GuardUp = "^"
+  show GuardDown = "v"
+  show Free = "."
+  show Obstacle = "#"
+  show Patrolled = "X"
+
+newtype Grid = Grid [[Object]] deriving (Eq)
+
+instance Show Grid where
+  -- show (Grid g) = intercalate "\n" $ map (concatMap show) g
+  show (Grid rows) = unlines (map (concatMap show) rows)
 
 parseObject :: Char -> Object
 parseObject '.' = Free
@@ -14,8 +31,11 @@ parseObject _ = error "invalid object"
 
 main = do
   raw_text <- getContents
-  let grid = (map . map) parseObject $ lines raw_text
-  print $ grid
+  let grid = Grid $ (map . map) parseObject $ lines raw_text
+  print grid
+
+walkUntilLeave :: Grid -> Int
+walkUntilLeave g = 0
 
 leavingArea :: Grid -> Bool
 leavingArea g = False
