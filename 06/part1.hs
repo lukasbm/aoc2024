032123271129f949790a2a1ecf826867f8b40a2c
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-07 04:19:07 +0100

    progres  on d6p1

--- 06/part1.hs
+++ 06/part1.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 
-import Data.List (intercalate)
+import Data.List (find, intercalate)
+import Data.Maybe (isJust, isNothing)
 
 data Object = GuardLeft | GuardRight | GuardUp | GuardDown | Free | Obstacle | Patrolled deriving (Eq)
 
@@ -19,6 +20,9 @@ instance Show Grid where
   -- show (Grid g) = intercalate "\n" $ map (concatMap show) g
   show (Grid rows) = unlines (map (concatMap show) rows)
 
+-- instance Foldable Grid where
+--   foldMap f (Grid rows) = foldMap (foldMap f) rows
+
 parseObject :: Char -> Object
 parseObject '.' = Free
 parseObject '#' = Obstacle
@@ -29,16 +33,29 @@ parseObject '^' = GuardUp
 parseObject 'X' = Patrolled
 parseObject _ = error "invalid object"
 
+isGuard :: Object -> Bool
+isGuard GuardDown = True
+isGuard GuardUp = True
+isGuard GuardLeft = True
+isGuard GuardRight = True
+isGuard _ = False
+
 main = do
   raw_text <- getContents
   let grid = Grid $ (map . map) parseObject $ lines raw_text
   print grid
 
 walkUntilLeave :: Grid -> Int
-walkUntilLeave g = 0
+walkUntilLeave g = if leavingArea g then 1 else 1 + walkUntilLeave (step g)
 
+-- assuming there always has to be a guard:
+-- when we cut of the border (top, left, right and bottom rows/cols) and don't find one --> guard was at the border
 leavingArea :: Grid -> Bool
-leavingArea g = False
+leavingArea (Grid g) =
+  let borders = head g ++ last g ++ concatMap (\row -> [head row, last row]) (init (tail g))
+   in isJust $ find isGuard borders
 
+
+-- TODO: the hardest part?
 step :: Grid -> Grid
 step g = g
