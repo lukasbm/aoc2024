426f2b6e54012074774c9b0a6c7ac347adae7207
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-10 02:20:22 +0100

    fix types

--- 08/part1.hs
+++ 08/part1.hs
@@ -1,13 +1,20 @@
 import System.Environment (getArgs)
 
-data CellValue = Free | Antinode Char | Antenna Char deriving (Show, Eq)
+type Frequency = Char
 
-type Cell = [CellValue]
+data Cell = Free | Antinode Frequency | Antenna Frequency deriving (Show, Eq)
 
-type Coord = (Int , Int)
+type Coord = (Int, Int)
+
+type Grid = [[Cell]]
 
 main = do
   args <- getArgs
   raw_text <- if length args == 1 then readFile (head args) else error "usage: ./program <file>"
   let grid = (map . map) (\x -> if x == '.' then Free else Antenna x) $ lines raw_text
   print $ grid
+
+-- gets all possible pairs of a frequency
+-- bidirectional, so both (a,b) AND (b,a) will be included)
+pairs :: Grid -> Frequency -> [Coord]
+pairs grid freq = []
