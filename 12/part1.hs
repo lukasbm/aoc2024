97b9d5c98ddf9b1d41d60073dc5bd6e7781da198
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-12 09:09:56 +0100

    almost working

--- 12/part1.hs
+++ 12/part1.hs
@@ -1,13 +1,15 @@
 import Data.Array.IArray
 import Data.Array.Unboxed
 import Data.List (nub)
+import Data.Set qualified as Set
 import System.Environment (getArgs)
 
 type Coord = (Int, Int)
 
 type Grid = UArray Coord Char
 
-type Section = [(Coord, Int)]
+-- coords and perimeter values of each cell in a region
+type Region = [(Coord, Int)]
 
 -- Convert 2D list to UArray
 toUArray :: [[Char]] -> Grid
@@ -34,26 +36,34 @@ main = do
   raw_text <- if length args == 1 then readFile (head args) else error "usage: ./program <file>"
   let grid = toUArray $ lines raw_text :: Grid
 
-  print $ grid
-  print $ grid ! (2, 3)
-  print $ floodFill grid [] (2, 3)
+  -- print $ grid
+  -- print $ grid ! (2, 3)
+  print $ floodFill grid [] (2, 1)
+  print $ price $ floodFill grid [] (2, 1)
+
+  print $ solve grid
 
 -- perimeter sum * area
-price :: Section -> Int
+price :: Region -> Int
 price s = length s * sum (map snd s)
 
 -- each entry is a region
 solve :: Grid -> [Int]
-solve g = foldr (\regionPrices c -> price (floodFill g [] c) : regionPrices) [] (indices g)
-
--- i need the flood fill approach to get the area of each region ...
+solve g = map price $ go $ Set.fromList (indices g)
+  where
+    go :: Set.Set Coord -> [Region]
+    go xs
+      | Set.null xs = []
+      | otherwise =
+          let reg = floodFill g [] (Set.elemAt 0 xs)
+           in reg : go (Set.difference xs $ Set.fromList (map fst reg)) -- go again but with region removed
 
 -- each neighbor that is not of the same type adds 1 to the perimeter (for this cell)
 -- out of bounds also counts as a different type
 -- less than 4 coors on getNeighbors means out of bounds -> increase perimeter
 -- result: list of coors and their perimeter
 -- FIXME: wouldn't this cause overhead in calculation? as some subcalls might evaluate the same cell twice?
-floodFill :: Grid -> [Coord] -> Coord -> Section
+floodFill :: Grid -> [Coord] -> Coord -> Region
 floodFill grid visited pos =
   let sameNeighbors = filter (\x -> grid ! x == grid ! pos) $ getNeighbors grid pos
       perimeter = 4 - length sameNeighbors
