9718155b27de11bdc46016a3d4d55467344faf85
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-14 02:09:32 +0100

    lmao day 12 done

--- 12/part2.hs
+++ 12/part2.hs
@@ -7,6 +7,10 @@ import System.Environment (getArgs)
 
 type Coord = (Int, Int)
 
+b2i :: Bool -> Int
+b2i False = 0
+b2i True = 1
+
 type Grid = UArray Coord Char
 
 -- Convert 2D list to UArray
@@ -29,10 +33,6 @@ getNeighbors grid (i, j) =
       potentialNeighbors = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
    in filter (\(i, j) -> i >= minI && i <= maxI && j >= minJ && j <= maxJ) potentialNeighbors
 
--- neighbors in 4 directions with no bounds check
-getStupidNeighbors :: Grid -> Coord -> [Coord]
-getStupidNeighbors grid (i, j) = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
-
 main = do
   args <- getArgs
   raw_text <- if length args == 1 then readFile (head args) else error "usage: ./program <file>"
@@ -40,10 +40,11 @@ main = do
 
   -- print $ grid
   -- print $ grid ! (2, 3)
-  print $ floodFill grid [] (2, 1)
-  print $ price grid $ floodFill grid [] (2, 1)
-
--- print $ solve grid
+  -- let region_c = floodFill grid [] (2, 3)
+  -- print region_c
+  -- print $ sum $ map (corners grid region_c) region_c
+  -- print $ price grid $ region_c
+  print $ sum $ solve grid
 
 -- price = number of sides * area = number of corners * area
 price :: Grid -> [Coord] -> Int
@@ -55,15 +56,25 @@ price g s =
 -- number of valid corners at on position in a region
 -- assumes that x `elem` region
 corners :: Grid -> [Coord] -> Coord -> Int
-corners g region x =
-  let neigh = getNeighbors g x
-      -- diff is between 0 and 4 long
-      diff = Set.toList $ Set.difference (Set.fromList neigh) (Set.fromList region)
-      diff_pairs = (,head diff) <$> tail diff
-   in  length $ filter inLine diff_pairs
-  where
-    inLine :: (Coord, Coord) -> Bool
-    inLine ((a1, a2), (b1, b2)) = b1 - a1 == 0 || b2 - a2 == 0
+corners g region (r, c) =
+  let reg = Set.fromList region
+      top = (r - 1, c)
+      left = (r, c - 1)
+      right = (r, c + 1)
+      bottom = (r + 1, c)
+      topLeft = (r - 1, c - 1)
+      topRight = (r - 1, c + 1)
+      bottomLeft = (r + 1, c - 1)
+      bottomRight = (r + 1, c + 1)
+      cornerOutsideTopLeft = Set.notMember top reg && Set.notMember left reg
+      cornerOutsideTopRight = Set.notMember top reg && Set.notMember right reg
+      cornerOutsideBottomLeft = Set.notMember bottom reg && Set.notMember left reg
+      cornerOutsideBottomRight = Set.notMember bottom reg && Set.notMember right reg
+      cornerInsideTopLeft = Set.member top reg && Set.member left reg && Set.notMember topLeft reg
+      cornerInsideTopRight = Set.member top reg && Set.member right reg && Set.notMember topRight reg
+      cornerInsideBottomLeft = Set.member bottom reg && Set.member left reg && Set.notMember bottomLeft reg
+      cornerInsideBottomRight = Set.member bottom reg && Set.member right reg && Set.notMember bottomRight reg
+   in b2i cornerOutsideBottomLeft + b2i cornerOutsideBottomRight + b2i cornerOutsideTopLeft + b2i cornerOutsideTopRight + b2i cornerInsideBottomLeft + b2i cornerInsideBottomRight + b2i cornerInsideTopLeft + b2i cornerInsideTopRight
 
 solve :: Grid -> [Int]
 solve g = map (price g) $ go $ Set.fromList (indices g)
