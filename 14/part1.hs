897630473079c5a1a57198a4d9e6be0e79265862
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-15 05:58:40 +0100

    robot move

--- 14/part1.hs
+++ 14/part1.hs
@@ -9,7 +9,7 @@ data Robot = Robot {pos :: Coord, vel :: Coord} deriving (Eq, Show)
 
 atoi :: String -> Int
 atoi "" = error "empty string"
-atoi x = read $ filter isDigit x
+atoi x = read $ filter (\x -> isDigit x || x == '-') x
 
 parseRobot :: String -> Robot
 parseRobot r =
@@ -20,14 +20,19 @@ parseRobot r =
 
 -- we index (x,y), where x is number of tile from the left wall and y is number of rows from the top!
 -- positive x means moving right, positive y means moving down
+-- grid is 0 indexed!
 
 main = do
   args <- getArgs
   raw <- if length args == 1 then readFile (head args) else error "usage: ./program <file>"
   let size = if head args == "input.txt" then (101, 103) else (11, 7) :: Coord
   let robots = map parseRobot (lines raw)
-  -- print $ robots
-  print $ size
+  print $ take 5 $ iterate (moveRobot size) (head robots)
+
+-- moves the robot according to its own velocity
+-- will wrap around the grid if border is reached!
+moveRobot :: Coord -> Robot -> Robot
+moveRobot (width, height) (Robot (px, py) (vx, vy)) = Robot ((px + vx) `mod` width, (py + vy) `mod` height) (vx, vy)
 
 step :: [Robot] -> [Robot]
 step rs = []
