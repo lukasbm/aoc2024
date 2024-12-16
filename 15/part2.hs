8631f9825244e6d2d59b91d2260168fd047c6b92
Lukas Boehm <lukas.l.boehm@fau.de>
2024-12-16 02:54:21 +0100

    start part 2 and fix parser

--- 15/part2.hs
+++ 15/part2.hs
@@ -5,11 +5,12 @@ import Prelude hiding (Left, Right)
 
 type Coord = (Int, Int)
 
-data WarehouseCell = Free | Wall | Box | Robot deriving (Eq)
+data WarehouseCell = Free | Wall | BoxLeft | BoxRight | Robot deriving (Eq)
 
 instance Show WarehouseCell where
   show Wall = "#"
-  show Box = "O"
+  show BoxLeft = "["
+  show BoxRight = "]"
   show Robot = "@"
   show Free = "."
 
@@ -19,14 +20,14 @@ type Warehouse = Array Coord WarehouseCell
 parseWarehouse :: [[Char]] -> Warehouse
 parseWarehouse c =
   let height = length c
-      width = length $ head c
-   in listArray ((0, 0), (height - 1, width - 1)) $ map parseWarehouseCell (concat c)
+      width = 2 * length (head c)
+   in listArray ((0, 0), (height - 1, width - 1)) $ concatMap parseWarehouseCell (concat c)
 
-parseWarehouseCell :: Char -> WarehouseCell
-parseWarehouseCell '@' = Robot
-parseWarehouseCell '#' = Wall
-parseWarehouseCell 'O' = Box
-parseWarehouseCell '.' = Free
+parseWarehouseCell :: Char -> [WarehouseCell]
+parseWarehouseCell '@' = [Robot, Free]
+parseWarehouseCell '#' = [Wall, Wall]
+parseWarehouseCell 'O' = [BoxLeft, BoxRight]
+parseWarehouseCell '.' = [Free, Free]
 parseWarehouseCell c = error $ "invalid cell: " <> show c
 
 prettyPrint :: Array (Int, Int) WarehouseCell -> String
@@ -66,21 +67,21 @@ main = do
 
   print moves
   print robot_pos
-  -- print "warehouse initial"
-  -- putStrLn $ prettyPrint $ warehouse
+  print "warehouse initial"
+  putStrLn $ prettyPrint $ warehouse
 
-  let warehouse_moved = snd $ foldl (\(robot_pos, w) move -> trace ("doing a move: " <> show move <> " on pos " <> show robot_pos) $ step w move robot_pos) (robot_pos, warehouse) moves
-  putStrLn $ prettyPrint $ warehouse_moved
-  print $ sum $ coordinates warehouse_moved
+  -- let warehouse_moved = snd $ foldl (\(robot_pos, w) move -> trace ("doing a move: " <> show move <> " on pos " <> show robot_pos) $ step w move robot_pos) (robot_pos, warehouse) moves
+  -- putStrLn $ prettyPrint $ warehouse_moved
+  -- print $ sum $ coordinates warehouse_moved
 
 coordinates :: Warehouse -> [Int]
-coordinates g = map (\(y, x) -> x + 100 * y) $ filterIndices (== Box) g
+coordinates g = map (\(y, x) -> x + 100 * y) $ filterIndices (== BoxLeft) g
 
 step :: Warehouse -> Move -> Coord -> (Coord, Warehouse)
 step warehouse move robot_pos
   | warehouse ! next_robot_pos == Wall = (robot_pos, warehouse)
   | warehouse ! next_robot_pos == Free = (next_robot_pos, moveRobot warehouse)
-  | warehouse ! next_robot_pos == Box && canPush = (next_robot_pos, moveRobot $ pushBoxes warehouse move next_robot_pos)
+  | warehouse ! next_robot_pos == BoxLeft && canPush = (next_robot_pos, moveRobot $ pushBoxes warehouse move next_robot_pos)
   | otherwise = (robot_pos, warehouse)
   where
     next_robot_pos = nextPos move robot_pos
@@ -91,12 +92,12 @@ step warehouse move robot_pos
     directionCells dir pos = case warehouse ! pos of
       Wall -> []
       Free -> [Free]
-      Box -> Box : directionCells dir (nextPos dir pos)
+      BoxLeft -> BoxLeft : directionCells dir (nextPos dir pos)
 
     pushBoxes :: Warehouse -> Move -> Coord -> Warehouse
     pushBoxes w m pos
       | w ! pos == Free = w
-      | otherwise = pushBoxes w m new_pos // [(pos, Free), (new_pos, Box)]
+      | otherwise = pushBoxes w m new_pos // [(pos, Free), (new_pos, BoxLeft)]
       where
         new_pos = nextPos m pos
 
